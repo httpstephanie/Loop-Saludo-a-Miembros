8714e2bc01d2410dc6bca6bc5ac57c7c2b8113ae
httpstephanie <110868588+httpstephanie@users.noreply.github.com>
2022-10-20 13:14:57 -0500

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,7 @@
+#Utilizando loops For, saluda a todos los miembros de una clase, imprimiendo "Hola" + su nombre.
+#Por ejemplo: "Hola María"
+#alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+ 
+for alumno in alumnos_clase:
+    print(f"Hola {alumno}")
